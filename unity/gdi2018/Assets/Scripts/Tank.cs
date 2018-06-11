f9851c892b999c8f613bf9c0863d9ae6707ec360
Sava Minic <savam@nordeus.com>
2018-06-12 00:01:54 +0200

    Base tank script and MonoBehaviour

---
+++ unity/gdi2018/Assets/Scripts/Tank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+	private void Awake()
+	{
+		Debug.Log("Awake " + gameObject.name);
+	}
+
+	private void Start()
+	{
+		Debug.Log("Start " + gameObject.name);
+	}
+
+	private void Update()
+	{
+		Debug.Log("Update " + transform.position);
+	}
+
+	private void OnDestroy()
+	{
+		Debug.Log("OnDestroy");
+	}
+}
