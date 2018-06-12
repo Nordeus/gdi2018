b65297c2ab1b8e4af418f3b8ad316ecd61974a9b
Sava Minic <savam@nordeus.com>
2018-06-13 00:50:23 +0200

    shooting tower is rotating

---
+++ unity/gdi2018/Assets/Scripts/ShootingTower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTower : MonoBehaviour
+{
+
+	[SerializeField]
+	private float towerRotationSpeed;
+
+	private Tank targetTank;
+
+	private void Start()
+	{
+		targetTank = FindObjectOfType<Tank>();
+	}
+
+	private void Update()
+	{
+		if (targetTank == null)
+			return;
+
+		var towerToTank = targetTank.transform.position - transform.position;
+		towerToTank.y = 0f;
+		var rotation = Quaternion.LookRotation(towerToTank);
+		
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * towerRotationSpeed);
+	}
+
+}
