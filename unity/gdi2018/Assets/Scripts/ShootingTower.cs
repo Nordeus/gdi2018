713d6f59699216448d6a3eecbd22a5502d22bf7e
Sava Minic <savam@nordeus.com>
2018-06-20 00:30:37 +0200

    Tower shooting at tank

---
+++ unity/gdi2018/Assets/Scripts/ShootingTower.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTower : MonoBehaviour
+{
+
+	[SerializeField]
+	private float towerRotationSpeed;
+
+	[SerializeField]
+	private float reloadDuration;
+	
+	[SerializeField]
+	private float shootingForce;
+
+	[SerializeField]
+	private Transform shootingFrom;
+
+	private float timeToReload;
+
+	private Tank targetTank;
+
+	private void Awake()
+	{
+		timeToReload = 0f;
+	}
+
+	private void Start()
+	{
+		targetTank = FindObjectOfType<Tank>();
+	}
+
+	private void Update()
+	{
+		if (targetTank == null)
+			return;
+
+		var towerToTank = targetTank.transform.position - transform.position;
+		towerToTank.y = 0f;
+		var rotation = Quaternion.LookRotation(towerToTank);
+		
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * towerRotationSpeed);
+
+		if (timeToReload > 0)
+		{
+			timeToReload -= Time.deltaTime;
+		}
+
+		if (timeToReload <= 0)
+		{
+			Shoot();
+		}
+	}
+
+	private void Shoot()
+	{
+		timeToReload = reloadDuration;
+		
+		var bulletGameobject = Instantiate(Resources.Load("Prefabs/CompleteShell")) as GameObject;
+		
+		bulletGameobject.transform.position = shootingFrom.position;
+		bulletGameobject.transform.forward = transform.forward;
+		bulletGameobject.GetComponent<Rigidbody>().AddForce(shootingForce * bulletGameobject.transform.forward);
+	}
+
+}
